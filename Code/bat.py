a153544f7520e18888f310928b9b9568f7aeb2b8
plaidlemming <81821336+plaidlemming@users.noreply.github.com>
2021-05-17 18:14:16 +0100

    Set map boundary and roosting rules

--- Code/bat.py
+++ Code/bat.py
@@ -6,36 +6,97 @@ Created on Sat May 15 13:18:54 2021
 """
 import random
 
-
 colour = ["white","red", "green", "blue"]
 
 class NewBat():
     def __init__(self, habitat, bats):
-        self.y = random.randint(0,100)
-        self.x = random.randint(0,100)
+        self.y = random.randint(0,99)
+        self.x = random.randint(0,99)
         
+        #self.y = 98
+        #self.x = 98
         
+        self.roosted = False
         self.habitat = habitat
         self.bats = bats
         self.species = random.randint(1,3)
         self.colour = colour[self.species]
         
-        #print(self.habitat[self.y][self.x])
+        # testing: print(self.habitat[self.y][self.x])
         
-        #print(self.colour)
     def fly(self):
-        # Check to see if the current habitat under the bat
-        # has the same value as the bat species
-        # If is is different then keep moving 
-        # Other wise stay still (do nothing)
-        #if not self.habitat[self.y][self.x] == self.species:
+        if self.roosted == False:
+            validMove = False
+     
         
-            if random.random() < 0.5:
-                self.y = (self.y + 1) % 100
-            else:
-                self.y = (self.y - 1) % 100
+
+            while (validMove == False):
+                if random.random() < 0.5:
+                    newY = (self.y + 1)
+                else:
+                    newY = (self.y - 1)
+                    
+                if random.random() < 0.5:
+                    newX = (self.x + 1)
+                else:
+                    newX = (self.x - 1)
+            
+                print (newX, newY, self.habitat[newY][newX])
+                if 0 <= newY <= 99 and 0 <= newX <= 99:
+                    if self.habitat[newY][newX] == self.species:
+                        
+                        self.y = newY
+                        self.x = newX
+                        
+                        print ("new home who dis")
+                        
+                        self.roosted = True
+                        validMove = True
+                    else:
+                        self.y = newY
+                        self.x = newX
+                        
+                        print ("inside border not home tho :(")
+                        validMove = True
+                else:
+                    print ("not in border wyd here bro")
+               
+            
                 
-            if random.random() < 0.5:
-                self.x = (self.x + 1) % 100
-            else:
-                self.x = (self.x - 1) % 100
+# make some bats avoid some habitat
+# red = woodland
+# green = woodland edge
+# blue = building
+
+# chose a new x and y value
+# check to see if it's in the bounds of the map
+# check to see if the environment at that point is something the bat avoids
+# check to see if it's the roosting habitat for that bat
+
+
+
+# setting roosting habitat in 'fly' function pre-while loop  
+# if not self.habitat[self.y][self.x] == self.species:          
+''' implement later
+def getx(self):
+    return self._x
+    
+    def setx(self, VALUE):
+        self._x = VALUE
+        
+    def delx(self):
+        del self._x
+        
+    #x = property(getx, setx, delx, "i'm the x property")
+    
+    def gety(self):
+        return self._y
+    
+    def sety(self, VALUE):
+        self._y = VALUE
+        
+    def dely(self):
+        del self._x
+        
+    #y = property(gety, sety, dely, "i'm the y property")
+'''
--- Code/model.py
+++ Code/model.py
@@ -9,15 +9,14 @@ import csv
 import matplotlib.pyplot as plot
 import bat
 import matplotlib.animation as animate
-import time
 
 num_of_bats = 10
-num_of_iterations = 10
+num_of_iterations = 100
 
 habitat = []
 bats = []
 
-file = open('environment.csv', newline='')
+file = open('habitat.csv', newline='')
 reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
 
 for row in reader:
@@ -29,69 +28,23 @@ file.close()
 
 fig = plot.figure(figsize=(100, 100))
 ax = fig.add_axes([0, 0, 1, 1])
-
-
-#plot.ylim(0, 100)
-#plot.xlim(0, 100)
-
-
-plot.imshow(habitat)
-plot.show()
-
     
 # make bat
 for i in range(num_of_bats):
     bats.append(bat.NewBat(habitat, bats))
-    #print(bats[i].colour)
+    #testing: print(bats[i].colour)
     plot.scatter(bats[i].x, bats[i].y, c = bats[i].colour, s = 10)
-    #plot.scatter(bats[i].x,bats[i].y)
            
-        
+# move bat        
 def update(frame_number):
-    #fig.clear()
-    
-    #plot.imshow(habitat)
-    for j in range (num_of_bats):
+    fig.clear()
+    plot.imshow(habitat)
+    for i in range (num_of_bats):
         bats[i].fly()
-        
-        plot.scatter(bats[i].x, bats[i].y, c = bats[i].colour, s = 10)
+        plot.scatter(bats[i].x, bats[i].y, c = bats[i].colour, s = 10,)
+        plot.show()
+        #testing: print("i work")
     
-    '''
-    for j in range(100):
-        for k in range (100):
-            plot.scatter(k,j,habitat[k][j])
- 
-    '''
+animation = animate.FuncAnimation(fig, update, interval=100, repeat=False, frames=num_of_iterations)
 
-'''    
-for j in range (num_of_iterations):
-    for i in range(num_of_bats):
-        bats[i].fly()
-    time.sleep(1)
-    print("i work")
-'''  
-'''  
-plot.scatter(bats[i].x, bats[i].y, c = bats[i].colour, s = 10)
-plot.show
-update()
-'''
-'''  
-update()
-time.sleep(1)
-update()
-time.sleep(1)
-update()
-time.sleep(1)
-update()
-time.sleep(1)
-update()
-time.sleep(1)
-'''
-'''
-for k in range(10):
-    update()
-    time.sleep(1)
-'''   
-    
-#animation = animate.FuncAnimation(fig, update, interval=1000, repeat=False, frames=num_of_iterations)
-#plot.show()
+

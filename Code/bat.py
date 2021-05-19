f96fa7298087a8c975e53d082a033aafb4eb6b77
plaidlemming <81821336+plaidlemming@users.noreply.github.com>
2021-05-19 23:52:45 +0100

    Updates

--- Code/bat.py
+++ Code/bat.py
@@ -9,9 +9,9 @@ import random
 colour = ["white","red", "green", "blue"]
 
 class NewBat():
-    def __init__(self, habitat, hawks):
-        self.y = random.randint(0,99)
-        self.x = random.randint(0,99)
+    def __init__(self, habitat, hawks, infected = False):
+        self.y = random.randint(0,49)
+        self.x = random.randint(0,49)
         
         #self.y = 98
         #self.x = 98
@@ -21,44 +21,46 @@ class NewBat():
         self.hawks = hawks
         self.species = random.randint(1,3)
         self.colour = colour[self.species]
+        self.infected = infected
+        self.alive = True
         
         # testing: print(self.habitat[self.y][self.x])
         
     def fly(self):
-        if self.roosted == False:
-            validMove = False
-
-            while (validMove == False):
-                if random.random() < 0.5:
-                    newY = (self.y + 1)
-                else:
-                    newY = (self.y - 1)
-                    
-                if random.random() < 0.5:
-                    newX = (self.x + 1)
-                else:
-                    newX = (self.x - 1)
-            
-                #print (newX, newY, self.habitat[newY][newX])
-                if 0 <= newY <= 99 and 0 <= newX <= 99:
-                    if self.habitat[newY][newX] == self.species:
-                        
-                        self.y = newY
-                        self.x = newX
-                        
-                        #print ("new home who dis")
-                        
-                        self.roosted = True
-                        validMove = True
+        if self.alive == True:
+            if self.roosted == False:
+                validMove = False
+    
+                while (validMove == False):
+                    if random.random() < 0.5:
+                        newY = (self.y + 1)
                     else:
-                        self.y = newY
-                        self.x = newX
+                        newY = (self.y - 1)
                         
-                        #print ("inside border not home tho :(")
-                        validMove = True
-                #else:
-                    #print ("not in border wyd here bro")
-
+                    if random.random() < 0.5:
+                        newX = (self.x + 1)
+                    else:
+                        newX = (self.x - 1)
+                
+                    #print (newX, newY, self.habitat[newY][newX])
+                    if 0 <= newY <= 49 and 0 <= newX <= 49:
+                        if self.habitat[newY][newX] == self.species:
+                            
+                            self.y = newY
+                            self.x = newX
+                            
+                            #print ("new home who dis")
+                            
+                            self.roosted = True
+                            validMove = True
+                        else:
+                            self.y = newY
+                            self.x = newX
+                            
+                            #print ("inside border not home tho :(")
+                            validMove = True
+                    #else:
+                        #print ("not in border wyd here bro")
 
 
 
--- Code/hawk.py
+++ Code/hawk.py
@@ -8,12 +8,21 @@ Created on Mon May 17 22:25:22 2021
 import random
 
 class NewHawk():
-    def __init__(self, habitat):
-        self.y = random.randint(0,99)
-        self.x = random.randint(0,99)
+    def __init__(self, habitat, bats):
+        self.y = 0
+        self.x = 0
         self.habitat = habitat
+        self.bats = bats
+        self.find_woodland()
+        self.alive = True
+        
+    def update(self):
+        self.fly()  
+        self.find_and_eat_bats()
     
+    # Move hawks depending on a random number, as long as they're within the bounds of the map and within woodland
     def fly(self):
+        if self.alive == True:
             validMove = False
 
             while (validMove == False):
@@ -27,9 +36,38 @@ class NewHawk():
                 else:
                     newX = (self.x - 1)
 
-                if 0 <= newY <= 99 and 0 <= newX <= 99:
+                if 0 <= newY <= 49 and 0 <= newX <= 49:
+                     if self.habitat[newY][newX] == 1:
                    
-                    self.y = newY
-                    self.x = newX
-                    
-                    validMove = True
+                        self.y = newY
+                        self.x = newX
+                        
+                        validMove = True
+            
+    def find_and_eat_bats(self):
+        for bat in self.bats:
+           if self.x == bat.x and self.y == bat.y:
+              #if random.random() < 0.7:
+                  if bat.infected == True:
+                    self.alive = False
+                    bat.alive = False
+                    #print("yummy bat")
+
+    # Choose new coordinates and check if it is woodland, if not, choose another set until it is, then move there
+    def find_woodland(self):
+        validMove = False
+        
+        while (validMove == False):
+            newX = random.randint(0,49)
+            newY = random.randint(0,49)
+            
+            if self.habitat[newY][newX] == 1:
+                self.x = newX
+                self.y = newY
+                # print("i got wood ladies")
+                validMove = True
+
+
+            
+            
+            
